f570bab857e7814cc570e97e02cece3c746c0429
harishankards <harishankards@gmail.com>
2017-06-01 15:56:18 +0530

    Add bootstrap and jquery and import it

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ class App extends Component {
   render() {
     return (
       <div>
-        Hello world
+        <div className= "panel panel-default">
+          <div className="panel-body">
+            Hello world
+          </div>
+        </div>
       </div>
     );
   }
