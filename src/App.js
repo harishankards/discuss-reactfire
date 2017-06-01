62c4f4a041a06ac9d6dcf1c868c6699836616cba
harishankards <harishankards@gmail.com>
2017-06-01 17:18:28 +0530

    Add function addPost and event handler

--- src/App.js
+++ src/App.js
@@ -6,12 +6,31 @@ class App extends Component {
   constructor(props){
     super(props);
 
+    this.addPost = this.addPost.bind(this);
+    this.handlePostEditorInput = this.handlePostEditorInput.bind(this);
+
     this.state = {
       posts: [
-        'Hello dudes!'
+        'Hello dudes! Default post'
       ]
     }
   }
+
+  addPost(){
+    const newState = Object.assign({}, this.state);
+    newState.posts.push(this.state.newPostBody);
+    newState.newPostBody = '';
+    this.setState(newState);
+  }
+
+  handlePostEditorInput(event){
+    this.setState({
+      newPostBody: event.target.value
+    })
+    }
+
+
+
   render() {
     return (
       <div>
@@ -25,8 +44,8 @@ class App extends Component {
 
         <div className= "panel panel-default post-editor">
           <div className="panel-body">
-            <textarea className="form-control post-editor-input" />
-              <button className="btn btn-success post-editor-button">Post</button>
+            <textarea className="form-control post-editor-input" onChange={this.handlePostEditorInput} />
+              <button className="btn btn-success post-editor-button" onClick={this.addPost}>Post</button>
           </div>
         </div>
       </div>
